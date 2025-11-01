94f24ae1068debcf507d25743a690b654d5f6b32
Nilbezer3 <nilbezer09@icloud.com>
2025-11-02 00:21:24 +0300

    Add Enum package

---
+++ backend/Service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.nilbezer.cs308.Service;
+
+import com.nilbezer.cs308.Model.Entity.UserEntity;
+import com.nilbezer.cs308.Repository.UserRepository;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+
+@Data
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmailAddress(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        GrantedAuthority authority =
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmailAddress(),
+                user.getPassword(),
+                Collections.singleton(authority)
+        );
+    }
+}
