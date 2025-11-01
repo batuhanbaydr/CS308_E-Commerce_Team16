1179c3fc0f0b84f2355173a862c8fc09f6ae6fb5
bekazanc <bekazanc@gmail.com>
2025-11-02 00:53:30 +0300

    Add Repository package

---
+++ backend/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package edu.sabanciuniv.cs308.Repository;
+
+import edu.sabanciuniv.cs308.Model.Entity.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+
+    boolean existsByEmailAddress(String emailAddress);
+
+    Optional<UserEntity> findByEmailAddress(String emailAddress);
+}
