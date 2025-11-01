4fc6098039d06ba2ebe7d1589745e19744f7809b
Nilbezer3 <nilbezer09@icloud.com>
2025-11-02 01:21:04 +0300

    Add Service packet

--- backend/Service/CustomUserDetailsService.java
+++ backend/Service/CustomUserDetailsService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
 
+
 @Data
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
