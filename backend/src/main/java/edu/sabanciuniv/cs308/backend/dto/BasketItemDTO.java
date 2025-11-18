9171464ac89d0c8ff95a48308d56cd67c388a030
Nilbezer3 <nilbezer09@icloud.com>
2025-11-18 14:56:01 +0300

    feat(basket): add BasketItemDTO

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/dto/BasketItemDTO.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/dto/BasketItemDTO.java
@@ -1,4 +1,16 @@
 package edu.sabanciuniv.cs308.backend.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class BasketItemDTO {
+
+    private String productId;
+    private String sku;
+    private String name;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal lineTotal;
 }
