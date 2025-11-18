5c028fc013a80b4767e3380b4af10f9a582765be
Nilbezer3 <nilbezer09@icloud.com>
2025-11-18 14:57:01 +0300

    feat(basket): add UpdateBasketItemRequest

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/request/UpdateBasketItemRequest.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/request/UpdateBasketItemRequest.java
@@ -1,4 +1,11 @@
 package edu.sabanciuniv.cs308.backend.request;
 
+import lombok.Data;
+
+@Data
 public class UpdateBasketItemRequest {
+
+    private String productId;
+    private String sku;
+    private int quantity;   // 0 veya altı gelirse item’i sileriz
 }
