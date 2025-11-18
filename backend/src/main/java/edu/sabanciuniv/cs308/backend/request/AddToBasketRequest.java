cc6c6bb3423ceea9bdd3b85a4c8880364bc375c5
Nilbezer3 <nilbezer09@icloud.com>
2025-11-18 14:56:32 +0300

    feat(basket): add AddToBasketRequest

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/request/AddToBasketRequest.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/request/AddToBasketRequest.java
@@ -1,4 +1,11 @@
 package edu.sabanciuniv.cs308.backend.request;
 
+import lombok.Data;
+
+@Data
 public class AddToBasketRequest {
+
+    private String productId;   // ProductEntity.id
+    private String sku;         // ProductEntity.Variant.sku
+    private int quantity;
 }
