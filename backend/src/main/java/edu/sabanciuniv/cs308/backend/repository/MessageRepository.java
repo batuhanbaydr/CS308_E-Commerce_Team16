0ca01798545476a0d4cf0ef3c96bb62f0f487465
Nilbezer3 <nilbezer09@icloud.com>
2026-01-02 00:37:38 +0300

    Add MessageRepository for chat messages

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/repository/MessageRepository.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/repository/MessageRepository.java
@@ -1,4 +1,11 @@
 package edu.sabanciuniv.cs308.backend.repository;
 
-public class MessageRepository {
+import edu.sabanciuniv.cs308.backend.entity.MessageEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends MongoRepository<MessageEntity, String> {
+
+    List<MessageEntity> findByConversationIdOrderByTimestampAsc(String conversationId);
 }
