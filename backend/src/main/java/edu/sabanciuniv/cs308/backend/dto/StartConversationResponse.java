bc3dfe9293f6224a27adeccdbb4d3b7251f5de87
Nilbezer3 <nilbezer09@icloud.com>
2026-01-02 00:37:04 +0300

    Add StartConversationResponse DTO

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/dto/StartConversationResponse.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/dto/StartConversationResponse.java
@@ -1,4 +1,11 @@
 package edu.sabanciuniv.cs308.backend.dto;
 
 public class StartConversationResponse {
+    private String conversationId;
+
+    public StartConversationResponse() {}
+    public StartConversationResponse(String conversationId) { this.conversationId = conversationId; }
+
+    public String getConversationId() { return conversationId; }
+    public void setConversationId(String conversationId) { this.conversationId = conversationId; }
 }
