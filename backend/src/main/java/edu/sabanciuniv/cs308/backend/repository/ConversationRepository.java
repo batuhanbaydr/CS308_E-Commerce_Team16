54ca537619958a863af3b325964895897bb725ef
Nilbezer3 <nilbezer09@icloud.com>
2026-01-02 00:37:32 +0300

    Add ConversationRepository for chat

--- backend/src/main/java/edu/sabanciuniv/cs308/backend/repository/ConversationRepository.java
+++ backend/src/main/java/edu/sabanciuniv/cs308/backend/repository/ConversationRepository.java
@@ -1,4 +1,18 @@
 package edu.sabanciuniv.cs308.backend.repository;
 
-public class ConversaitonRepository {
+import edu.sabanciuniv.cs308.backend.entity.ConversationEntity;
+import edu.sabanciuniv.cs308.backend.enums.ConversationStatus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ConversationRepository extends MongoRepository<ConversationEntity, String> {
+
+    List<ConversationEntity> findByStatusOrderByUpdatedAtDesc(ConversationStatus status);
+
+    List<ConversationEntity> findByStatusInOrderByUpdatedAtDesc(List<ConversationStatus> statuses);
+
+    List<ConversationEntity> findByUserIdOrderByUpdatedAtDesc(String userId);
+
+    List<ConversationEntity> findByGuestSessionIdOrderByUpdatedAtDesc(String guestSessionId);
 }
